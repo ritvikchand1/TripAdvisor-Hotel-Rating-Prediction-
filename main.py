030701ad6b8eb7a035fa6823eb0be861b5da2ad6
ritvikchand1 <rchand@ucsd.edu>
2023-09-07 14:01:07 -0700

    flask website builds

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request
+import ml
+
 app = Flask(__name__)
 
 @app.route("/")
 def render_home():
-   return render_template('index.html')
+   return render_template("index.html")
 
-@app.route("/submit_input_text", methods = ['GET', 'POST'])
-def process_input():
-    input = request.get_json()
-    print(input)
-    print(input['text'])
-    return input['text']
+@app.route("/predict", methods = ["GET", "POST"])
+def predict():
+    if request.method == "POST": 
+        print(request.method)
+        review_text = request.form.get("review-content")
+
+        # predictions
+        pred_rating = ml.pred(review_text)
+        return render_template("index.html", rating = pred_rating)
+    else:
+        print(request.method)
+        return render_template("index.html", rating = 0)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port = 8080)
+    app.run(host='0.0.0.0', port = 8080, debug=True)
